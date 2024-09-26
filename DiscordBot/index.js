a350b7bc20b4c326b315f5fcb079e9a32be59286
AnujChhikara <anujchhikara777@gmail.com>
2024-09-26 17:48:55 +0530

    bot slash command

--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -1,15 +1,30 @@
-const {Client, GatewayIntentBits}  = require("discord.js")
+const { Client, GatewayIntentBits, ConnectionService } = require("discord.js");
 
-const client = new Client({intents:[
-    GatewayIntentBits.Guilds, 
-    GatewayIntentBits.GuildMessages, 
-    GatewayIntentBits.MessageContent]})
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
-client.on('messageCreate', (message) =>{
-    if(message.author.bot) return;
-    message.reply({
-        content:"hi from bot"
-    })
-})
+client.on("interactionCreate", (interaction) => {
+  console.log(interaction);
+  interaction.reply("pong!!");
+});
 
-client.login("Discord Secret")
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith("create")) {
+    const url = message.content.split("create")[1];
+    return message.reply({
+      content: "Generating short url..." + url,
+    });
+  }
+  message.reply({
+    content: "hi from bot",
+  });
+});
+
+client.login("discord token");
