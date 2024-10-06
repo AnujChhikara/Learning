c275d3de0ca030a7a18ea77595d5b646d64b4ca6
AnujChhikara <anujchhikara777@gmail.com>
2024-10-06 11:10:36 +0530

    basic models structure

--- node/socket.io/projects/backend/src/app.js
+++ node/socket.io/projects/backend/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 const app = express();
+import router from "./routes/index.js";
 
 app.use(express.json());
 app.use(cors());
+
+app.use(router)
 
 export default app;
---
+++ node/socket.io/projects/backend/src/models/projects.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  tasks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+
+});
+
+const Project = mongoose.model("Project", projectSchema);
+
+export default Project;
